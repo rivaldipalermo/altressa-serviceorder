ccc7d38612eb785ed53b37cf012838216d60e57a
Rivaldi Palermo <rivaldi.palermo11@gmail.com>
2022-01-22 22:22:22 +0700

    initial project order service

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(Request $request)
+    {
+        $userId = $request->query('user_id');
+        $order = Order::query();
+
+        $order->when($userId, function($query) use ($userId) {
+            return $query->where('user_id', '=', $userId);
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order->get()
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $user = $request->user;
+        $course = $request->course;
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id']
+        ]);
+
+        $transactionDetails = [
+            'order_id' => $order->id,
+            'gross_amount' => $course['price']
+        ];
+
+        $itemDetails = [
+            [
+                'id' => $course['id'],
+                'price' => $course['price'],
+                'quantity' => 1,
+                'name' => $course['name'],
+                'brand' => 'Learnzone',
+                'category' => 'Online Course'
+            ]
+        ];
+
+        $customerDetails = [
+            'first_name' => $user['name'],
+            'email' => $user['email']
+        ];
+
+        $midtransParams = [
+            'transaction_details' => $transactionDetails,
+            'item_details' => $itemDetails,
+            'customer_details' => $customerDetails
+        ];
+
+        $midtransUrl = $this->getMidtransSnapUrl($midtransParams);
+
+        $order->snap_url = $midtransUrl;
+        $order->metadata = [
+            'course_id' => $course['id'],
+            'course_price' => $course['price'],
+            'course_name' => $course['name'],
+            'course_thumbnail' => $course['thumbnail'],
+            'course_level' => $course['level']
+        ];
+        
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order
+        ]);
+    }
+
+    private function getMidtransSnapUrl($params)
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_IS_PRODUCTION');
+        \Midtrans\Config::$isSanitized = true;
+        \Midtrans\Config::$is3ds = true;
+
+        $snapToken = \Midtrans\Snap::createTransaction($params)->redirect_url;
+        return $snapToken;
+    }
+}
